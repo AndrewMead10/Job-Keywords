2e8f33676a666f0a07dc06f9bce8e6c60938481c
AndrewMead10 <55472544+AndrewMead10@users.noreply.github.com>
2020-09-16 14:12:34 -0400

    Removed unnecessary document

--- random tetsing.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import psycopg2
-
-#dat = datetime.date.today().strftime("%B_%d_%Y")
-
-#print(dat)
-
-connect = psycopg2.connect('dbname = jobs user=postgres password = amqm2001')
-cursor = connect.cursor()
